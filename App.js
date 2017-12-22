d5adcb2a679e7e287a14ce39fc31eb1c82d6ffa2
Ben Buttigieg <benbtg@live.com>
2017-12-22 16:53:15 +0000

    Added NavigatorIOS (Not displaying main page!)

--- App.js
+++ App.js
@@ -11,19 +11,42 @@ import {
   Text,
   View,
   TabBarIOS,
-  TextInput
+  NavigatorIOS,
+  TextInput,
+  TouchableHighlight
 } from 'react-native';
 
+
+class SearchPage extends Component<{}> {
+  render() {
+    return(
+    <View style>
+    <Text>Current Scene: { this.props.title }</Text>
+    <TouchableHighlight>
+      <Text>Tap me to load the next scene</Text>
+    </TouchableHighlight>
+  </View>);
+  }
+}
+
 export default class App extends Component<{}> {
-  render() { return React.createElement(Text, {style: styles.description}, "MC Weather Btg");
+  render() {
+    return (
+      <NavigatorIOS
+        style={{flex: 1}}
+        initialRoute={{
+          title: 'MC Weather',
+          component: SearchPage,
+        }}/>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   description: {
-    fontSize: 18,
+    fontSize: 20,
     textAlign: 'center',
-    color: '#F56565',
-    marginTop: 65,
+    color: '#656565',
+    marginTop: 20,
   },
 });
