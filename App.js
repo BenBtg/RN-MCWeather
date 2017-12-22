e067cbcf4e0abc580bd066a6deec80ca50dc2bdc
Ben Buttigieg <benbtg@live.com>
2017-12-22 13:41:49 +0000

    Updated bundle ID and Certs

--- App.js
+++ App.js
@@ -9,49 +9,21 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  TabBarIOS,
+  TextInput
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          MC Weather!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+  render() { return React.createElement(Text, {style: styles.description}, "MC Weather Btg");
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 30,
+  description: {
+    fontSize: 18,
     textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    color: '#F56565',
+    marginTop: 65,
   },
 });
