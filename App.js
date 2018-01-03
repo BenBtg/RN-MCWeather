f5d42cac43838d831ab0ee07d0d98af28fcea2ef
Ben Buttigieg <benbtg@live.com>
2018-01-03 14:09:44 +0000

    Recreated with "Create React Native App"

--- App.js
+++ App.js
@@ -1,68 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-'use strict';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Image,
-  Text,
-  View,
-  TabBarIOS,
-  NavigatorIOS,
-  TextInput,
-  TouchableHighlight,
-  ScrollView
-} from 'react-native';
-
-
-class SearchPage extends Component<{}> {
-  render() {
-    return(
-    <ScrollView backgroundColor="green">
-        <Image
-          source={{uri: 'https://www.planwallpaper.com/static/images/1379596.jpg'}}
-          style={{width: 320, height:180}}
-        />
-        <Text>
-          On iOS, a React Native ScrollView uses a native UIScrollView.
-          On Android, it uses a native ScrollView.
-
-          On iOS, a React Native Image uses a native UIImageView.
-          On Android, it uses a native ImageView.
-
-          React Native wraps the fundamental native components, giving you
-          the performance of a native app, plus the clean design of React.
-        </Text>
-      </ScrollView>
-  );
-  }
-}
-
-export default class App extends Component<{}> {
+export default class App extends React.Component {
   render() {
     return (
-      <NavigatorIOS
-        style={{flex: 1}}
-        initialRoute={{
-          title: 'MC Weather Test',
-          component: SearchPage,
-          rightButtonTitle: 'Add',
-          onRightButtonPress: this.onRightButtonPress
-        }}/>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  description: {
-    fontSize: 20,
-    textAlign: 'center',
-    color: '#656565',
-    marginTop: 20,
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
