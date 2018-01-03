2cfc24eacbc22b37875df066e5c8f8c4fc0bb91d
Ben Buttigieg <benbtg@live.com>
2018-01-03 13:31:22 +0000

    Some UI

--- App.js
+++ App.js
@@ -3,29 +3,43 @@
  * https://github.com/facebook/react-native
  * @flow
  */
+'use strict';
 
 import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
+  Image,
   Text,
   View,
   TabBarIOS,
   NavigatorIOS,
   TextInput,
-  TouchableHighlight
+  TouchableHighlight,
+  ScrollView
 } from 'react-native';
 
 
 class SearchPage extends Component<{}> {
   render() {
     return(
-    <View>
-    <Text>Current Scene: { this.props.title }</Text>
-    <TouchableHighlight>
-      <Text>Tap me to load the next scene</Text>
-    </TouchableHighlight>
-  </View>);
+    <ScrollView backgroundColor="green">
+        <Image
+          source={{uri: 'https://www.planwallpaper.com/static/images/1379596.jpg'}}
+          style={{width: 320, height:180}}
+        />
+        <Text>
+          On iOS, a React Native ScrollView uses a native UIScrollView.
+          On Android, it uses a native ScrollView.
+
+          On iOS, a React Native Image uses a native UIImageView.
+          On Android, it uses a native ImageView.
+
+          React Native wraps the fundamental native components, giving you
+          the performance of a native app, plus the clean design of React.
+        </Text>
+      </ScrollView>
+  );
   }
 }
 
@@ -35,8 +49,10 @@ export default class App extends Component<{}> {
       <NavigatorIOS
         style={{flex: 1}}
         initialRoute={{
-          title: 'MC Weather',
+          title: 'MC Weather Test',
           component: SearchPage,
+          rightButtonTitle: 'Add',
+          onRightButtonPress: this.onRightButtonPress
         }}/>
     );
   }
