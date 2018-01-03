269e1c97bf77b28ed8ef90d519a804a02ff4c19d
Ben Buttigieg <benbtg@live.com>
2018-01-03 17:09:48 +0000

    WIP Layout

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default class App extends React.Component {
+class Greeting extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Text Style>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
+
+
+export default class MCWeatherApp extends React.Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+
+    return (
+        <View style={styles.container}>
+        <Image/>
+        <Greeting name='Ben'/>
+          <Text>Open up App.js to sart working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+          
+          <Image style={{width: 193, height: 110}}
+          source={pic}/>
+
+        </View>
     );
   }
 }
@@ -16,8 +36,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#99f',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-end',
   },
 });
